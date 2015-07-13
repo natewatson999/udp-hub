ff677dfabcdd3242699f580efbb5d623b93f35a4
Nate Watson <watsonn2013@my.fit.edu>
2015-07-13 13:24:41 -0400

    minimally usable server
    The server now works somewhat.

--- udp-hub.js
+++ udp-hub.js
@@ -1,3 +1,40 @@
 var dgram = require("dgram");
+var events = require("events");
 var output = {};
+function createSocket(callback){
+	this.emitter = new events.EventEmitter();
+	this.server4 = dgram.createSocket("udp4");
+	this.server6 = dgram.createSocket("udp6");
+	if (callback) {
+		callback(this.emitter);
+	}
+	return this.emitter;
+};
+createSocket.prototype.bind = function(port, callback){
+	server4.on("message", function(message, remote){
+		this.emmiter.emit("message", message, remote)
+	});
+	server6.on("message", function(message, remote){
+		this.emmiter.emit("message", message, remote)
+	});
+	this.server4.on("listening", function(){
+		this.server6.on("listening", function(){
+			this.emitter.emit("listening");
+		});
+		this.server6.bind({
+			port: port,
+			address: "::0",
+			exclusive: false	
+		});
+	});
+	this.server4.bind({
+		port: port,
+		address: "0.0.0.0",
+		exclusive: false	
+	});
+	return;
+};
+output.createSocket = function(callback){
+	return new createSocket(callback);
+};
 module.exports = exports = output;
