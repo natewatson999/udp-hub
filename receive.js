5f23a3fd8152ccc9a637da230d9173ba29b7454f
Nate Watson <watsonn2013@my.fit.edu>
2015-07-29 12:10:36 -0400

    receive now uses internal sockets
    createReceiver has been rewritten and uses the internal version of
    sockets instead of a custom system. The documentation is the same.

--- receive.js
+++ receive.js
@@ -1,27 +1,22 @@
 var dgram = require("dgram");
+var udp = require("./socket.js");
 var ipFormat = require("./addressLogic").ipFormat;
 var output = {};
 var createReceiver = function (){
-	this.client6 = dgram.createSocket("udp6");
-	this.client4 = dgram.createSocket("udp4");
-	var self = this;
+	this.socket = udp.createSocket();
 	this.on = function(event, callback) {
 		switch(event) {
 			case "message":
-				self.client6.on("message", callback);
-				self.client4.on("message", callback);
+				this.socket.on("message", callback);
 				break;
 			case "listening":
-				self.client6.on("listening", function(){
-					self.client4.on("listening", callback);
-				});
+				this.socket.on("listening", callback);
 				break;
 			case "error":
-				self.client6.on("error", callback);
-				self.client4.on("error", callback);
+				this.socket.on("error", callback);
 				break;
 			case "close":
-				self.client6.on("close", callback);
+				this.socket.on("close", callback);
 				break;
 			default:
 				break;
@@ -29,56 +24,23 @@ var createReceiver = function (){
 	};
 };
 createReceiver.prototype.send = function(message, start, end, port, address, callback){
-	if (ipFormat(address)=="IPv6") {
-		this.client6.send(message, start, end, port, address, callback);
-	} else {
-		this.client4.send(message, start, end, port, address, callback);
-	}
+	this.socket.send(message, start, end, port, address, callback);
 	return;
 };
 createReceiver.prototype.close = function(callback) {
-	var self = this;
-	this.client4.close(function(){
-		self.client6.close(function(){
-			if (callback) {
-				callback();
-			}
-		});
-	});
+	if (callback) {
+		this.on("close", callback);
+	}
+	this.socket.close();
 };
 createReceiver.prototype.setTTL = function(count) {
-	this.client6.setTTL(count);
-	this.client4.setTTL(count);
+	this.socket.setTTL(count);
 };
 createReceiver.prototype.addMembership = function(address, interface) {
-	if (ipFormat(address)=="IPv6") {
-		if (interface) {
-			this.client6.addMembership(address, interface);
-		} else {
-			this.client6.addMembership(address);
-		}
-	} else {
-		if (interface) {
-			this.client4.addMembership(address, interface);
-		} else {
-			this.client4.addMembership(address);
-		}
-	}
+	this.socket.addMembership(address, interface);
 };
 createReceiver.prototype.dropMembership = function(address, interface) {
-	if (ipFormat(address)=="IPv6") {
-		if (interface) {
-			this.client6.dropMembership(address, interface);
-		} else {
-			this.client6.dropMembership(address);
-		}
-	} else {
-		if (interface) {
-			this.client4.dropMembership(address, interface);
-		} else {
-			this.client4.dropMembership(address);
-		}
-	}
+	this.socket.dropMembership(address, interface);
 };
 output.createReceiver = function(){
 	return new createReceiver();
